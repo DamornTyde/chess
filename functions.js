0863be6b92b2b64580418bc6b66ff359f9a2c74a
Damorn Tyde <34279265+DamornTyde@users.noreply.github.com>
2020-12-02 22:46:22 +0100

    Update

--- functions.js
+++ functions.js
@@ -176,6 +176,51 @@ function drawQueen(colour, outline) {
 const blckqn = drawQueen("#000", "#fff");
 const whtqn = drawQueen("#fff", "#000");
 
+//king
+function drawKing(colour, outline) {
+	const temp = createCanvas(grid, grid);
+    const tmpCtx = temp.getContext("2d");
+    tmpCtx.beginPath();
+	tmpCtx.moveTo(scalePoint(25), scalePoint(90));
+	tmpCtx.quadraticCurveTo(scalePoint(25), scalePoint(65), scalePoint(10), scalePoint(30));
+	tmpCtx.quadraticCurveTo(scalePoint(45), scalePoint(90), scalePoint(45), scalePoint(30));
+	tmpCtx.lineTo(scalePoint(35), scalePoint(30));
+	tmpCtx.lineTo(scalePoint(35), scalePoint(20));
+	tmpCtx.lineTo(scalePoint(45), scalePoint(20));
+	tmpCtx.lineTo(scalePoint(45), scalePoint(10));
+	tmpCtx.lineTo(scalePoint(55), scalePoint(10));
+	tmpCtx.lineTo(scalePoint(55), scalePoint(20));
+	tmpCtx.lineTo(scalePoint(65), scalePoint(20));
+	tmpCtx.lineTo(scalePoint(65), scalePoint(30));
+	tmpCtx.lineTo(scalePoint(55), scalePoint(30));
+	tmpCtx.quadraticCurveTo(scalePoint(55), scalePoint(90), scalePoint(90), scalePoint(30));
+	tmpCtx.quadraticCurveTo(scalePoint(75), scalePoint(65), scalePoint(75), scalePoint(90));
+	fillshape(tmpCtx, colour, outline);
+	return temp;
+}
+
+const blckKng = drawKing("#000", "#fff");
+const whtKng = drawKing("#fff", "#000");
+
+//pawn
+function drawPawn(colour, outline) {
+	const temp = createCanvas(grid, grid);
+    const tmpCtx = temp.getContext("2d");
+    tmpCtx.beginPath();
+    tmpCtx.moveTo(scalePoint(10), scalePoint(90));
+    tmpCtx.quadraticCurveTo(scalePoint(45), scalePoint(80), scalePoint(45), scalePoint(40));
+    tmpCtx.bezierCurveTo(scalePoint(40), scalePoint(40), scalePoint(40), scalePoint(35), scalePoint(45), scalePoint(35));
+    tmpCtx.quadraticCurveTo(scalePoint(25), scalePoint(10), scalePoint(50), scalePoint(10));
+    tmpCtx.quadraticCurveTo(scalePoint(75), scalePoint(10), scalePoint(55), scalePoint(35));
+    tmpCtx.bezierCurveTo(scalePoint(60), scalePoint(35), scalePoint(60), scalePoint(40), scalePoint(55), scalePoint(40));
+    tmpCtx.quadraticCurveTo(scalePoint(55), scalePoint(80), scalePoint(90), scalePoint(90));
+	fillshape(tmpCtx, colour, outline);
+	return temp;
+}
+
+const blckPwn = drawPawn("#000", "#fff");
+const whtPwn = drawPawn("#fff", "#000");
+
 //classes
 class player {
     constructor(name) {
@@ -242,6 +287,12 @@ class coor {
 //build game
 const players = [new player("White"), new player("Black")];
 
+function placePawns(p, c, a) {
+	for (x = 0; x < 8; x++) {
+		players[p].pieces.push(new pawn(new coor(x, c), a));
+	}
+}
+
 //white player
 players[0].pieces.push(new rook(new coor(0, 7), whtRk));
 players[0].pieces.push(new rook(new coor(7, 7), whtRk));
@@ -250,6 +301,8 @@ players[0].pieces.push(new knight(new coor(6, 7), whtknght));
 players[0].pieces.push(new bishop(new coor(2, 7), whtBshp));
 players[0].pieces.push(new bishop(new coor(5, 7), whtBshp));
 players[0].pieces.push(new queen(new coor(3, 7), whtqn));
+players[0].pieces.push(new king(new coor(4, 7), whtKng));
+placePawns(0, 6, whtPwn);
 
 //black player
 players[1].pieces.push(new rook(new coor(0, 0), blckRk));
@@ -259,6 +312,8 @@ players[1].pieces.push(new knight(new coor(6, 0), blckknght));
 players[1].pieces.push(new bishop(new coor(2, 0), blckBshp));
 players[1].pieces.push(new bishop(new coor(5, 0), blckBshp));
 players[1].pieces.push(new queen(new coor(3, 0), blckqn));
+players[1].pieces.push(new king(new coor(4, 0), blckKng));
+placePawns(1, 1, blckPwn);
 
 drawGame();
 
