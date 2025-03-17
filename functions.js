a1b5a11fd6812b5b74b2e1114fcff2e35a8e93e9
Ronald van Zon <34279265+DamornTyde@users.noreply.github.com>
2025-03-17 22:15:39 +0100

    update

--- functions.js
+++ functions.js
@@ -1010,12 +1010,12 @@ function bot() {
     const own = players[whosTurn(false)].pieces;
     const playerPieces = players[whosTurn(false)].pieces;
     for (let i of playerPieces) {
-        const pieceAttack = i.move(true).flat();
+        const pieceAttack = i.move(false).flat();
         const enemyAttack = getAttackGrid(i.pos);
         const ownAttack = getAttackGrid(i.pos, false);
         const base = getBasePoints(enemy, ownAttack) - getBasePoints(own, enemyAttack) - getPositionPoints(i, ownAttack) -
             getBasePoints(pieceAttack, enemy) - getBasePoints(pieceAttack, own) + getPositionPoints(i, enemyAttack);
-        const pieceMoves = pieceAttack.map(x => new movement(i.pos, x));
+        const pieceMoves = i.move(true).flat().map(x => new movement(i.pos, x));
         for (let m of pieceMoves) {
             //
         }
