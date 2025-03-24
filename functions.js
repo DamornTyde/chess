192fa0f10d6993aa6620430eda583e702f85aba2
Ronald van Zon <34279265+DamornTyde@users.noreply.github.com>
2025-03-24 21:33:25 +0100

    update

--- functions.js
+++ functions.js
@@ -537,13 +537,14 @@ function buildPlayers() {
     const playerNames = ["White", "Black"];
     for (let i = 0; i < 2; i++) {
         const temp = new player(playerNames[i]);
+        const side = 7 - 7 * i;
         for (let i2 = 0; i2 < 2; i2++) {
-            temp.pieces.push(new rook(new coor(0 + 7 * i2, 7 - 7 * i), rookAsset[i]));
-            temp.pieces.push(new knight(new coor(1 + 5 * i2, 7 - 7 * i), knightAsset[i]));
-            temp.pieces.push(new bishop(new coor(2 + 3 * i2, 7 - 7 * i), bishopAsset[i]));
+            temp.pieces.push(new rook(new coor(0 + 7 * i2, side), rookAsset[i]));
+            temp.pieces.push(new knight(new coor(1 + 5 * i2, side), knightAsset[i]));
+            temp.pieces.push(new bishop(new coor(2 + 3 * i2, side), bishopAsset[i]));
         }
-        temp.pieces.push(new queen(new coor(3, 7 - 7 * i), queenAsset[i]));
-        temp.pieces.push(new king(new coor(4, 7 - 7 * i), kingAsset[i]));
+        temp.pieces.push(new queen(new coor(3, side), queenAsset[i]));
+        temp.pieces.push(new king(new coor(4, side), kingAsset[i]));
         for (let x = 0; x < 8; x++) {
             temp.pieces.push(new pawn(new coor(x, 6 - 5 * i), pawnAsset[i], -1 + 2 * i));
         }
